e47e9f8146a75b4d5f3735e19352e9de281e7147
Mohamed Khaled <109686132+m7md5303@users.noreply.github.com>
2026-02-19 00:44:37 +0200

    Functional Coverage Report Generating Script

---
+++ cov.tcl
@@ -0,0 +1,2 @@
+xcrg -report_format html -dir ./veer_uvm.sim/sim_1/behav/xsim/xsim.covdb/top_uvm_behav/xsim.covinfo -report_dir ./
+exec echo "Coverage HTML Report and Wavedata VCD File were generated successfully"
